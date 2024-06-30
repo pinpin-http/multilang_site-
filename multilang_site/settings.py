bbbb8e316fc57e610f860cec65c1049e5089a381
I’m a teapot <51910093+pinpin-http@users.noreply.github.com>
2024-06-30 17:24:47 +0200

    Update settings.py

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,10 +92,7 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.config(conn_max_age=600)
 }
 
 
